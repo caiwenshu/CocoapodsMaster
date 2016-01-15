2eef5de32ebe3ccde9ffc1c4ad77790d138ac2c8
caiwenshu <shenma@yidianzudemacbook-pro-4.local>
2016-01-15 18:12:01 +0800

    Initial Commit

---
+++ CocoapodsMaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoapodsMaster
+//
+//  Created by shenma on 1/15/16.
+//  Copyright © 2016 shenma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoapodsMaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoapodsMaster
+//
+//  Created by shenma on 1/15/16.
+//  Copyright © 2016 shenma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
