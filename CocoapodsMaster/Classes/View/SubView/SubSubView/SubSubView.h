6507d0fa2dec0731473b83840d5bd4da3c00d6ce
caiwenshu <shenma@yidianzudeMacBook-Pro-4.local>
2016-01-18 15:59:30 +0800

    0.0.3

---
+++ CocoapodsMaster/Classes/View/SubView/SubSubView/SubSubView.h
@@ -0,0 +1,13 @@
+//
+//  SubSubView.h
+//  CocoapodsMaster
+//
+//  Created by shenma on 1/18/16.
+//  Copyright © 2016 shenma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubSubView : UIView
+
+@end
---
+++ CocoapodsMaster/Classes/View/SubView/SubSubView/SubSubView.m
@@ -0,0 +1,20 @@
+//
+//  SubSubView.m
+//  CocoapodsMaster
+//
+//  Created by shenma on 1/18/16.
+//  Copyright © 2016 shenma. All rights reserved.
+//
+
+#import "SubSubView.h"
+
+@implementation SubSubView
+
+
+-(void)helloWorld
+{
+    NSLog(@"current version: %@",@"0.0.3");
+    NSLog(@"Hello World!");
+}
+
+@end
